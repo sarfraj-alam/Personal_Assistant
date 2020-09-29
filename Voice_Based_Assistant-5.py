8bc7f3f586376f3479740e3aca65dbe259b1cb1b
Sarfraj Alam <62060382+sarfraj-alam@users.noreply.github.com>
2020-09-29 16:34:02 +0530

    Initial Commit

---
+++ Voice_Based_Assistant-5.py
@@ -0,0 +1,23 @@
+import speech_recognition as sr
+import webbrowser
+
+print("Welcome")
+
+#print("Please Speak what you want  : ", end = '')
+#ch = input()
+
+r = sr.Recognizer()
+
+with sr.Microphone() as source : # 'with' keyword is used to connect Python with Microphone and store the variable named as 'source '
+    print("Start Saying, we are listening....")
+    audio  =r.listen(source) # listen() will listen the voice and store into the variable named as 'audio'
+    print("We got your requirement, please Wait!")
+
+ch = r.recognize_google(audio)
+
+if (("run" or "start" or "launch") and ("Linux" in ch)):
+	webbrowser.open("http://192.168.43.239/mypage.html")
+elif (("exit" or "quit") in ch):
+	exit()
+else:
+	print("Wrong choice")
